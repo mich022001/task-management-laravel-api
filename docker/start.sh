ff164015e7a71354ee382e3628cec06c73ca69c4
mich022001 <02michael2001@gmail.com>
2026-08-04 18:54:01 +0800

    fix(deployment): skip view cache for API-only app

--- docker/start.sh
+++ docker/start.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env sh
 set -eu
 
-php artisan package:discover --ansi
 php artisan config:cache
 php artisan route:cache
-php artisan view:cache
 
 php-fpm -D
 exec nginx -g "daemon off;"
